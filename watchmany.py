d64d8d0bf24daddd0d8c94ba673da750b3314ac0
Andrew Chow <achow101-github@achow101.com>
2021-05-02 19:17:02 -0400

    More pools

--- watchmany.py
+++ watchmany.py
@@ -7,6 +7,16 @@ import signal
 POOLS = [
     ["stratum+tcp://us-east.stratum.slushpool.com:3333", "achow101.worker1:pass"],
     ["stratum+tcp://btc.viabtc.com:3333", "achow101.001:123"],
+    ["stratum+tcp://btc-na.f2pool.com:3333", "achow102.001:21235365876986800"],
+    ["stratum+tcp://stratum.1thash.btc.top:8888", "achow102.wentaproot:wentaproot"],
+    ["stratum+tcp://us.ss.btc.com:1800", "achow101.wentaproot:wentaproot"],
+    ["stratum+tcp://ss.antpool.com:3333", "achow101.wentaproot:wentaproot"],
+    ["stratum+tcp://stratum.kano.is:3333", "achow101.wentaproot:x"],
+    [
+        "stratum+tcp://solo.ckpool.org:3333",
+        "bc1qmuf9u75g745955f67c85nd33pdyh4v8zzr2lms.wentaproot:x",
+    ],
+    ["stratum+tcp://btc.ss.poolin.com:443", "achow101.001:123"],
 ]
 
 parser = argparse.ArgumentParser(
